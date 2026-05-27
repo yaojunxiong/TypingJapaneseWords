03b4170aa618277e75101d22778c00b5053f4270
yaojunxiong <yaojunxiong@outlook.com>
2026-05-27 18:31:44 +0900

    Ignore generated deployment output in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTs];
+const eslintConfig = [
+  {
+    ignores: [".next/**", ".vercel/**", "node_modules/**"],
+  },
+  ...nextVitals,
+  ...nextTs,
+];
 
 export default eslintConfig;
